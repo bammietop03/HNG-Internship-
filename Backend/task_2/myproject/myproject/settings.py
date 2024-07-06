05a8438695468d3f2b2661e6afc69b5e6c4d093b
Bamiwo Adebayo <bammietop@gmail.com>
2024-07-06 17:26:22 +0100

    HNG-task2

--- Backend/task_2/myproject/myproject/settings.py
+++ Backend/task_2/myproject/myproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#c4%)zxv^zz6(c!3ayn+6qi15eoknp+o065%3=1-p94=6kd#x#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hng-internship-8flj.onrender.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
